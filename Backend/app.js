3124a1f1d05291940f08536e1d3906681df689c2
husamkamal <husamkmal94@gmail.com>
2022-11-06 21:05:34 +0200

    deploy

--- Backend/app.js
+++ Backend/app.js
@@ -1,3 +1,5 @@
+const { join } = require('path');
+
 const express = require('express');
 
 const app = express();
@@ -13,6 +15,13 @@ mongoose.connect(process.env.DB_URL)
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(join(__dirname, '..', 'FrontEnd', 'build')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(join(__dirname, '..', 'FrontEnd', 'build', 'index.html'));
+  });
+}
 app.use('/api/v1', router);
 
 module.exports = app;
